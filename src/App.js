8c57a513915c511301256ef7dff06afaf9dbdf96
RaounakLz <lazragraounak@gmail.com>
2022-05-18 12:15:43 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form, Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{height:"650px"}}>
+      <Form style={{width:"250px", marginTop:"20px", marginLeft:"50px"}}>
+  <Form.Group className="mb-3" controlId="formGroupEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formGroupPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Button class="btn btn-primary" type="submit" value="Submit"> submit</Button>
+
+  
+</Form>
     </div>
   );
 }
